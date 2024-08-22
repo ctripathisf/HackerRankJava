6c78def57f62d122959a0a62d517a970279cf8ee
chandan <ctripathi.sf@gmail.com>
2024-08-22 18:23:56 +0530

    second code

---
+++ stdin.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class stdin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number");
+		int a=scan.nextInt();
+		scan.close();
+		System.out.println(a);
+
+	}
+
+}
